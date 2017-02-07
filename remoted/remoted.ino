60981e33400c7d59755ab8c2069c89e0f0dd68cb
Eric Dand <eric.c.dand@gmail.com>
2017-02-07 00:27:58 -0800

    add remoted station code

---
+++ remoted/remoted.ino
@@ -0,0 +1,68 @@
+#include "scheduler.h"
+
+/* Notes:
+ *  The Mega 2560 has a 64-byte software serial buffer.
+ *  You can check if it overflowed with Serial.overflow().
+ */
+
+#define BUF_SIZE 16
+uint8_t rx_buf[BUF_SIZE];
+uint8_t bytes_available = 0;
+uint8_t tx_buf[BUF_SIZE];
+uint8_t bytes_to_send = 0;
+
+void read_bluetooth() {
+ digitalWrite(2, HIGH);
+  while(Serial1.available() && bytes_available < BUF_SIZE) {
+    rx_buf[bytes_available++] = Serial1.read();
+  }
+ digitalWrite(2, LOW);
+}
+
+void write_bluetooth() {
+  digitalWrite(3, LOW);
+  while(bytes_to_send-- > 0) {
+    Serial1.write(tx_buf[bytes_to_send]);
+  }
+ digitalWrite(3, LOW);
+}
+
+void setup() {
+  // Serial zero is used (sparingly!) for debug output to the PC.
+	Serial.begin(9600);
+  // The Bluetooth module should be connected to Serial1.
+  Serial1.begin(9600);
+  while(!Serial || !Serial1); // Wait for serial ports to be ready.
+
+  // These pins are used for monitoring using the logic analyzer.
+	pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+
+	Scheduler_Init();
+
+	// General form for starting a task:
+	// Scheduler_StartTask(offset, period, function_pointer);
+  Scheduler_StartTask(30, 100, read_bluetooth);
+  Scheduler_StartTask(60, 100, write_bluetooth);
+}
+
+void loop() {
+	uint32_t time_to_next_task = Scheduler_Dispatch();
+	if (time_to_next_task) {
+		digitalWrite(4, HIGH);
+		delay(time_to_next_task);
+		digitalWrite(4, LOW);
+	}
+}
+
+int main() {
+	init();
+	setup();
+
+	for(;;) {
+		loop();
+	}
+	return 0;
+}
+
